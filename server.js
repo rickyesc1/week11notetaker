0ef9870bd5494443583a1bdfdce6c3b97db9a711
Ricardo Crossley <richi.escamilla@gmail.com>
2022-01-16 23:09:06 -0800

    working on general requirements/testing

--- server.js
+++ server.js
@@ -14,4 +14,6 @@ app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
 // Start the server on the port
-app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
+app.listen(PORT, () => {
+    console.log(`Listening on PORT: ${PORT}`);
+});
